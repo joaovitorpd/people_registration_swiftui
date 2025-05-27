351d92403041c1094594293673516c229d9961b2
joaovitorpd <joaovitorpd@gmail.com>
2025-05-27 11:39:31 -0300

    fix: moved .gitignore to the right folder

---
+++ .gitignore.swift
@@ -0,0 +1,36 @@
+//
+//  .gitignore.swift
+//  people_registration_swiftui
+//
+//  Created by João Vitor Pires Dias on 27/05/25.
+//
+
+
+# Xcode build e cache
+DerivedData/
+build/
+*.xcuserstate
+
+# Configurações locais de usuário
+*.xcworkspace/xcuserdata/
+*.xcuserdatad/
+xcuserdata/
+
+# Swift Package Manager
+.build/
+.swiftpm/
+Package.resolved
+
+# Previews SwiftUI (Xcode 14.3+ / 15.4)
+.previews/
+
+# Arquivos de distribuição e debug
+*.xcarchive
+*.dSYM
+*.ipa
+
+# Arquivos de sistema macOS
+.DS_Store
+
+# IDEs externos (caso use algum JetBrains)
+.idea/
